3409fd49c3fc63776700b160ec4894a84e695fab
marcus.karrman <marcus.karrman@stendahls.se>
2023-11-16 11:56:33 +0100

    Added SSR version of movies/id with no caching

--- src/components/movies/index.tsx
+++ src/components/movies/index.tsx
@@ -2,18 +2,20 @@ import { Box, Typography } from "@mui/material"
 
 export type MoviesProps = {
   moviesData: any,
+  url: string,
 }
 
 
 const Movies = ({
   moviesData,
+  url,
 }: MoviesProps) => {
   const { films } = moviesData.data.allFilms;
 
   return (
     <Box display={"grid"} gap={4} mt={5} gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))" >
       {films.map((film, index) => (
-        <a href={`/movies/${film.id}`} key={film.id} style={{ color: "inherit" }}>
+        <a href={`${url}${film.id}`} key={film.id} style={{ color: "inherit" }}>
           <Box p={2} borderRadius={4} sx={{ backgroundColor: "#f5f5f5" }}>
             <img
               src={`https://source.unsplash.com/random/250x250/?star wars${Math.random()}}`}
